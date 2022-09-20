5df0aafe6dc463ebabaec17b0615a53547cce09d
tiga <2517789608@qq.com>
2022-09-20 18:14:58 +0800

    chart2 实时更新数据

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -6,9 +6,36 @@ import {px} from '../shared/px';
 // const px = (n) => n / 2420 * (window as any).pageWidth;
 export const Chart2 = () => {
   const divRef = useRef(null);
+  const myChart = useRef(null);
+  const data = [
+    {name: '城关区公安局', 2021: 1, 2022: 2},
+    {name: '七里河区公安局', 2021:3, 2022: 4},
+    {name: '西固区公安局', 2021: 5, 2022: 6},
+    {name: '安宁区公安局', 2021: 7, 2022: 8},
+    {name: '红古区公安局', 2021: 9, 2022:  10},
+    {name: '永登县公安局', 2021: 1, 2022: 2},
+    {name: '皋兰县公安局', 2021: 3, 2022: 5},
+    {name: '榆中县公安局', 2021:7, 2022: 9},
+    {name: '兰州新区公安局', 2021: 4, 2022: 6},
+  ];
   useEffect(() => {
-    const myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
+    setInterval(() => {
+      const newData = [
+        {name: '城关区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '七里河区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '西固区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '安宁区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '红古区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '永登县公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '皋兰县公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '榆中县公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+        {name: '兰州新区公安局', 2021: Math.random() * 10, 2022: Math.random() * 10},
+      ];
+      x(newData);
+    }, 3000);
+  }, []);
+  const x = (data) => {
+    myChart.current.setOption(createEchartsOptions({
         grid: {
           x: px(85),
           y: px(25),
@@ -24,18 +51,18 @@ export const Chart2 = () => {
         yAxis: {
           axisTick: {show: false},
           type: 'category',
-          data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局', '永登县公安局', '皋兰县公安局', '榆中县公安局', '兰州新区公安局'],
+          data: data.map(i => i.name),
           axisLabel: {
             formatter(val) {
               return val.replace('公安局', '\n公安局');
             },
-            lineHeight:px(14),
+            lineHeight: px(14),
           }
         },
         series: [{
           name: '2021年',
           type: 'bar',
-          data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          data: data.map(i => i[2021]),
           itemStyle: {
             normal: {
               color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
@@ -48,24 +75,28 @@ export const Chart2 = () => {
             }
           }
         },
-        {
-          name: '2022年',
-          type: 'bar',
-          data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
-          itemStyle: {
-            normal: {
-              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                offset: 0,
-                color: '#b92ae8'
-              }, {
-                offset: 1,
-                color: '#6773e7'
-              }]),
+          {
+            name: '2022年',
+            type: 'bar',
+            data: data.map(i => i[2022]),
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                  offset: 0,
+                  color: '#b92ae8'
+                }, {
+                  offset: 1,
+                  color: '#6773e7'
+                }]),
+              }
             }
-          }
-        }]
+          }]
       }
     ));
+  };
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current);
+    x(data);
   }, []);
 
   return (
