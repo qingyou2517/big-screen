d3123a882018036468bcccd475d352b1a2abe647
tiga <2517789608@qq.com>
2022-09-20 22:51:49 +0800

    实时更新 chart8: 犯罪年龄段

--- src/components/chart-8.tsx
+++ src/components/chart-8.tsx
@@ -2,13 +2,17 @@ import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
 import {createEchartsOptions} from '../shared/create-echarts-options';
 import {px} from '../shared/px';
+import {rand} from '../shared/rand';
 
 export const Chart8 = () => {
   const divRef = useRef(null);
+  const myChart=useRef(null)
   const colors = ['#856BED', '#F46064', '#F38E1C', '#1CDB7C', '#33A4FA'];
-  useEffect(() => {
-    const myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
+
+  const data=[{value: 17, name: '10-20'}, {value: 30, name: '20-30'},{value: 33, name: '30-40'}, {value: 16, name: '40-50'}, {value: 4, name: '50-60'},]
+
+  const x = (data) =>{
+    myChart.current.setOption(createEchartsOptions({
       color: colors,
       xAxis: {show: false},
       yAxis: {show: false},
@@ -21,7 +25,11 @@ export const Chart8 = () => {
           label: {
             show: true, position: 'inside', textStyle: {color: 'white', fontSize: px(20)},
             formatter(options) {
-              return (options.value * 100).toFixed(0) + '%';
+              let sum = 0;
+              for (let i = 0; i < data.length; i++) {
+                sum += data[i].value;
+              }
+              return (options.value / sum * 100).toFixed(0) + '%';
             }
           },
           labelLine: {show: false},
@@ -29,16 +37,28 @@ export const Chart8 = () => {
             borderColor: '#0F113A',
             borderWidth: px(4)
           },
-          data: [
-            {value: 0.17, name: '10-20'},
-            {value: 0.30, name: '20-30'},
-            {value: 0.33, name: '30-40'},
-            {value: 0.16, name: '40-50'},
-            {value: 0.04, name: '50-60'},
-          ]
+          data: data
         }
       ]
     }));
+  }
+
+  useEffect(()=>{
+    myChart.current = echarts.init(divRef.current);
+    x(data);
+  })
+  useEffect(() => {
+    setInterval(() => {
+      const createData = (data) => {
+        const array=rand(4,40,5)
+        for(let i=0;i<data.length;i++){
+          data[i].value=array[i]
+        }
+        return data
+      };
+      const newData = createData(data);
+      x(newData);
+    }, 3000);
   }, []);
 
   return (
